ee0efce702f60ea6037d6fe79c9f463db3eb8fe3
chusaubanh <166006147+chusaubanh@users.noreply.github.com>
2026-03-24 15:11:34 +0700

    Update for new repository: Smart-Work-App

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/SmartWorkApp/',
+  base: '/Smart-Work-App/',
 })
